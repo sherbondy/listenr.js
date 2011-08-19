78b34c8fea716039afc0c51591e61d6db626bf27
Ethan Sherbondy <sherbondye@gmail.com>
2011-08-19 00:56:43 -0700

    reorginizing project.

---
+++ secrets_placeholder.js
@@ -0,0 +1,9 @@
+var secrets = exports;
+
+/* To register a Tumblr API app, go here:
+*  http://www.tumblr.com/oauth/apps
+*/
+
+secrets.session_secret = 'make up a session secret here';
+secrets.oauth_consumer_key = 'YOUR TUMBLR CONSUMER KEY HERE',
+secrets.oauth_secret_key = 'YOUR TUMBLR SECRET KEY HERE';
